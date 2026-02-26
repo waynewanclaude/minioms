3c6e07601b55189e0ca13882424925c4378c28e7
wayne@emp-004 <wayne@emp-004>
2026-02-26 15:26:26 -0500

    ww:BUGFIX: raising bad exception

--- minioms/util/external_interface.py
+++ minioms/util/external_interface.py
@@ -8,6 +8,6 @@ def set_mktprc_loader(loader):
 
 def mktprc_loader():
 	if(__mktprc_loader__ is None):
-		raise "ERR: __mktprc_loader__ not set"
+		raise ValueError("ERR: __mktprc_loader__ not set")
 	return __mktprc_loader__
 
